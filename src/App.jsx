e7cb70782e35081cafb966cc77a8b2996fb3cd4a
Nisal Dilranga <dilranganisal@gmail.com>
2025-02-16 12:26:15 +0530

    remove wrong folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-
-import './App.css'
-import LoginPage from './Components/LoginPage';
-import SignUpPage from './Components/SignUpPage';
-import Home from './templates/Home'
+import "./App.css";
+import Cart from "./Components/cart/Cart";
+import Electronics from "./Components/categories/Electronics";
+import LoginPage from "./Components/LoginPage";
+import SignUpPage from "./Components/SignUpPage";
+import Home from "./templates/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CartProvider } from "react-use-cart";
 
 function App() {
-
-
   return (
     <>
-       <Router>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/signup" element={<SignUpPage/>} />
-      {/* <Route path="/signup" element={<SignupPage />} /> */}
-    </Routes>
-  </Router>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/signup" element={<SignUpPage />} />
+
+            <Route path="/categories/electronics" element={<Electronics />} />
+            <Route path="/cart" element={<Cart />} />
+
+            {/* <Route path="/signup" element={<SignupPage />} /> */}
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
